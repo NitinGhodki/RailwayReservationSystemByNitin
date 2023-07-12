a882f159065228fc46ef53b2e9ff04a73c75b7f4
Railwayreservation1 <sandeep810936@gmail.com>
2023-07-12 09:33:59 +0530

    welcome

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
-// import PNRStatus from './PNRStatus';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './component/Login';
 import Register from './component/Register';
 import Welcome from './component/welcome';
@@ -23,35 +22,29 @@ function App() {
 
         <Navbar id='Appnav'>
           <Container>
-            <Navbar.Brand href="/"  id =''><img src='/punlic/train logo3.png' alt='logo'id='apphomeimg'/></Navbar.Brand>
+            <Navbar.Brand href="/" id=''><img src='/punlic/train logo3.png' alt='logo' id='apphomeimg' /></Navbar.Brand>
             <Nav className="me-auto inernav">
-              {/* <Nav.Link href="/PNRStatus" className="appnavlink" id =''>PNR Status</Nav.Link> */}
-              <Nav.Link href="/SeatAvailable" className="appnavlink" id =''>Seat Availability</Nav.Link>
-              <Nav.Link href="/Fare Details" className="appnavlink" id =''>Fare Enquiry</Nav.Link>
-              {/* <Nav.Link href="/train bwetween station" className="appnavlink" id =''>Train Between Station</Nav.Link>  */}
-              {/* <Nav.Link href="/Train Schedule" className="appnavlink" id =''>Train Schedule</Nav.Link> */}
-              <Nav.Link href="/Forgetpassword" style={{display: 'none'}}></Nav.Link>
+              <Nav.Link href="/SeatAvailable" className="appnavlink" id=''>Seat Availability</Nav.Link>
+              <Nav.Link href="/Fare Details" className="appnavlink" id=''>Fare Enquiry</Nav.Link>
+              <Nav.Link href="/Forgetpassword" style={{ display: 'none' }}></Nav.Link>
             </Nav>
             <Nav className="ml-auto">
-              <Nav.Link href="/Login"><Button  className="appnavbutton">Login</Button>{' '}</Nav.Link>
-              <Nav.Link href="/Register"><Button  className="appnavbutton">Register</Button>{' '}</Nav.Link>
+              <Nav.Link href="/Login"><Button className="appnavbutton">Login</Button>{' '}</Nav.Link>
+              <Nav.Link href="/Register"><Button className="appnavbutton">Register</Button>{' '}</Nav.Link>
             </Nav>
           </Container>
         </Navbar>
-        {/* <Welcome/> */}
-        <Routes>
-          {/* <Route path="/PNRStatus" element={<PNRStatus />} /> */}
-          <Route path="/Login" element={<Login/>} />
 
+
+        <Routes>
+          <Route path="/Login" element={<Login />} />
           <Route path="/Register" element={<Register />} />
           <Route path="/" element={<Welcome />} />
           <Route path="/SeatAvailable" element={<SeatAvailable />} />
           <Route path="/Fare Details" element={<FareDetails />} />
           <Route path="/train bwetween station" element={<TrainBetweenStationInput />} />
           <Route path="/Train Schedule" element={<TrainSchedule />} />
-          <Route path='/Forgetpassword' element={<Forgetpassword/>} />
-
-
+          <Route path='/Forgetpassword' element={<Forgetpassword />} />
         </Routes>
       </Router>
     </div>
